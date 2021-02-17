f441e269376f21cabf1d8111ac09b85a5d613a03
pam <ana.osuna_vargas@etu.upmc.fr>
2021-02-17 20:58:39 +0100

    Added timer but needs to move with camera

---
+++ Assets/Scripts/ElapsedTime.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElapsedTime : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI text;
+    public float elapsedTime = 0f;
+    // Start is called before the first frame update
+    void Start(){
+        text = GetComponent<TMPro.TextMeshProUGUI>();
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        elapsedTime += Time.deltaTime;
+        text.text = "Elapsed time: " + Mathf.Round(elapsedTime);
+        
+    }
+}
