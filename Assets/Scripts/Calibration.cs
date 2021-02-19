5f0883c8dbbc7c0eb11116dee1eddcfc1116c264
pam <ana.osuna_vargas@etu.upmc.fr>
2021-02-19 10:02:45 +0100

    Preparing calibration

--- Assets/Scripts/Calibration.cs
+++ Assets/Scripts/Calibration.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class Calibration : MonoBehaviour
 {
-    TMPro.TextMeshProUGUI text;
-    Timer timer;
+    private TMPro.TextMeshProUGUI text;
+    private List<float> valuesForMoveThreshold;
+    private List<float> valuesForJumpThreshold;
+    private float secondsLeft;
 
     // Start is called before the first frame update
     void Start(){
         text = GameObject.Find("Canvas").GetComponentInChildren<TMPro.TextMeshProUGUI>();
-        timer = text.GetComponent<Timer>();
+        secondsLeft = 5;
     }
 
     // Update is called once per frame
     void Update(){
+        if (Input.GetKeyDown(KeyCode.Return)){
+            calibrate();
+        }
+
         
+    }
+
+    public void calibrate(){
+        text.text = "Generate sound with your average voice volume...\n"; 
+        Debug.Log("SecondsLeft: " + secondsLeft + "and" + secondsLeft*10);
+        while((secondsLeft > 0) && ((secondsLeft*10)%5 == 0)){
+            // get loudness value
+            text.text = Mathf.Round(secondsLeft) + "...";
+            Debug.Log("Saving loudness info");
+            // valuesForMoveThreshold.Add(loudnessValue);
+        }
+        // yield WaitForSeconds(5);
+
+        // now a little bit louder
+    }
+    public void startCountDown(){
+        secondsLeft -= Time.deltaTime; 
     }
 }
