6a21027641be435004388b809e732b47d2988d28
pam <ana.osuna_vargas@etu.upmc.fr>
2021-02-19 12:26:07 +0100

    Calibration works, need to add real loudness values now

--- Assets/Scripts/Calibration.cs
+++ Assets/Scripts/Calibration.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Calibration : MonoBehaviour
 {
@@ -8,11 +11,18 @@ public class Calibration : MonoBehaviour
     private List<float> valuesForMoveThreshold;
     private List<float> valuesForJumpThreshold;
     private float secondsLeft;
+    private IEnumerator coroutine;
+    private DateTime startingTime;
+    private float totalSeconds = 5f;
+    private bool moveThreshReady = false;
 
     // Start is called before the first frame update
     void Start(){
         text = GameObject.Find("Canvas").GetComponentInChildren<TMPro.TextMeshProUGUI>();
         secondsLeft = 5;
+        coroutine = SaveLoudnessValues();
+        valuesForMoveThreshold = new List<float>();
+        valuesForJumpThreshold = new List<float>();
     }
 
     // Update is called once per frame
@@ -20,24 +30,38 @@ public class Calibration : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return)){
             calibrate();
         }
+        if ((totalSeconds - (DateTime.Now - startingTime).TotalSeconds <= 0f) && (moveThreshReady)){
+            StopCoroutine(coroutine);
+            text.text = "Now a little bit louder...\n\n Press Enter when you are ready";
+        }
 
         
     }
 
     public void calibrate(){
-        text.text = "Generate sound with your average voice volume...\n"; 
-        Debug.Log("SecondsLeft: " + secondsLeft + "and" + secondsLeft*10);
-        while((secondsLeft > 0) && ((secondsLeft*10)%5 == 0)){
-            // get loudness value
-            text.text = Mathf.Round(secondsLeft) + "...";
-            Debug.Log("Saving loudness info");
-            // valuesForMoveThreshold.Add(loudnessValue);
+        startingTime = DateTime.Now;
+        if (!moveThreshReady){
+            text.text = "Generate sound with your average voice volume...\n"; 
         }
-        // yield WaitForSeconds(5);
-
+        // else{
+        //     text.text = "Now a little bit louder..."
+        // }
+        // Debug.Log("SecondsLeft: " + (totalSeconds - (DateTime.Now - startingTime).TotalSeconds) + "and" + secondsLeft*10);
+        moveThreshReady = true;
+        StartCoroutine(coroutine);
         // now a little bit louder
     }
-    public void startCountDown(){
-        secondsLeft -= Time.deltaTime; 
+
+    private IEnumerator SaveLoudnessValues(){
+        while(true){
+            // Debug.Log("Saving loudness info");
+            text.text = Mathf.Round(totalSeconds - (float)(DateTime.Now - startingTime).TotalSeconds) + "...";
+            if (!moveThreshReady){
+                valuesForMoveThreshold.Add(1.0f);
+            }else{
+                valuesForJumpThreshold.Add(2.0f);
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
